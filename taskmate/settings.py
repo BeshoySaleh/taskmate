a5f39d879d2e580f9d9e1a94cd8cb7716f73301e
BeshoySaleh <138182458+BeshoySaleh@users.noreply.github.com>
2024-02-19 20:31:46 +0200

    Update settings.py

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -21,8 +21,8 @@ SECRET_KEY = env("DJANGO_SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["*"]
-# CSRF_TRUSTED_ORIGINS = ['https://']
+ALLOWED_HOSTS = ["taskmate.4.us-1.fl0.io"]
+CSRF_TRUSTED_ORIGINS = ['https://taskmate.4.us-1.fl0.io']
 
 # Application definition
 
